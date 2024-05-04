e8bb14bcf92caa158d0c00eb4efe3d626bdc1a89
kabeer <studykabeer@gmail.com>
2024-05-04 17:32:33 +0530

    new compnents

---
+++ blogProject/src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Logo (){
+    
+        return (
+            <div>
+                
+            </div>
+        );
+}
+
+export default Logo;
--- blogProject/src/components/index.js
+++ blogProject/src/components/index.js
@@ -1,3 +1,6 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
-export {Header,Footer}
+import container from "./container/Container"
+import Logo from "./Logo";
+import Logoutbtn from "./header/LogoutBtn";
+export {Header,Footer,container,Logo,Logoutbtn}
